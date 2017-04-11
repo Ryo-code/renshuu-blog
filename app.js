cb2b2a9a38107d615c872eb80b85bda36e5dafcc
Ryo <kanadajinlee@yahoo.ca>
2017-04-11 18:32:04 -0500

    Pretty much can't move forward until I figure out MongoDB and mongoose. Urg...

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 // app.use(methodOverride("_method")); //Used to make the POST method in the edit file actually a PUT request (by overriding it)
 
+const tempTHINGSarray = [
+  {
+    title: "First Blog Post", 
+    image: "https://s-media-cache-ak0.pinimg.com/originals/1b/9c/de/1b9cded0bd00fe7cabe88a68a0b7aada.jpg", 
+    video: "f09876543", 
+    info: "This is the first blog post",
+  },
+  {
+    title: "Blog Post Numero Duce", 
+    image: "https://images2.alphacoders.com/467/467407.jpg", 
+    video: "adfsdaf", 
+    info: "This is the 2nd blog post",
+  },
+  {
+    title: "Post of the Third", 
+    image: "https://img.clipartfox.com/b76d78022cfb782b9727f33b49a1c5dd_1-10-vector-image-artistic-numbers-clipart_6621-3238.png", 
+    video: "grwadsaf", 
+    info: "This is the first blog post",
+  },
+];
+
+
 //Route 0: Root route
 app.get("/", (req, res) => {
   res.redirect("/things");
